c48aed93705cf9928e90515afdb71ca6529b7c56
zaamilion <107994046+zaamilion@users.noreply.github.com>
2023-07-04 16:12:21 +0000

    gg

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from routes import *
 from aiogram import types,Bot,Dispatcher,executor
 
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+
+from gtts import gTTS
+
 msgp = None
 msg = None
 markup_in = InlineKeyboardMarkup(row_width=3).row(InlineKeyboardButton(text='Спавн', callback_data='m_spawn_'),InlineKeyboardButton(text='exit',callback_data='exit')) 
 bot = Bot(token='6213197194:AAGzmFDDnZ-_Py1lgciZQiHfpGOopmNhqGs')
 dp = Dispatcher(bot)
 def markup_out(callin):
-    return  InlineKeyboardMarkup(row_width=3).row(InlineKeyboardButton(text='Спавн', callback_data=callin+'spawn_city_step0'),InlineKeyboardButton(text='Сцена Красногвардейск', callback_data=callin+'krasnogvardeysk_scene_step0'),InlineKeyboardButton(text='exit',callback_data='exit'))
+    return  InlineKeyboardMarkup(row_width=3).row(InlineKeyboardButton(text='Спавн', callback_data=callin+'spawn_city_pre'),InlineKeyboardButton(text='Сцена Красногвардейск', callback_data=callin+'krasnogvardeysk_scene_pre'),InlineKeyboardButton(text='exit',callback_data='exit'))
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
@@ -30,7 +33,7 @@ async def callb(call: types.CallbackQuery):
     if call.data == 'exit':
         await call.answer('restarted')
         msg = await call.message.answer('КУУУ, откуда едем?',reply_markup=markup_in)
-    if call.data[:1]=='m':
+    elif call.data[:1]=='m':
         print(1)
         try:
             step = int(call.data[-2]+call.data[-1])
@@ -43,7 +46,6 @@ async def callb(call: types.CallbackQuery):
         except:
             try:
                 step = int(call.data[-1])
-
                 try:
                     i = (globals()[call.data[:-6]])[step+1]
                     msgp = await call.message.answer_photo((globals()[(call.data[:-6])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Едем дальше',callback_data=call.data[:-1]+str(step+1))))
@@ -54,6 +56,8 @@ async def callb(call: types.CallbackQuery):
                 print(call.data[-3:])
                 if call.data[-3:]=='end':
                     msg = await call.message.answer('поездка успешно завершена',reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Начать новую поездку',callback_data='exit')))
+                elif call.data[-3:]=='pre':
+                    msgp = await call.message.answer_photo((globals()[call.data]),caption='Маршрут построен',reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Поехали',callback_data=call.data[:-3]+'step0'),InlineKeyboardButton(text='exit',callback_data='exit')))
                 else:
                     msg = await call.message.answer('а куда?',reply_markup=markup_out(call.data))
 
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 m_spawn_krasnogvardeysk_scene = [["spawn","utrecht"],["utrecht","krasnogvardeysk"],
                                  ["krasnogvardeysk","krasnogvardyesk"],["krasnogvardeysk","krasnogvardeysk_scene"]]
-m_spawn_krasnogvardeysk_scene_photo = ['https://i.ibb.co/X7k8bYz/IMG-20230703-221517-956.jpg',
-                                       'https://i.ibb.co/RBT65LS/IMG-20230703-221518-047.jpg',
-                                       'https://i.ibb.co/6wzzd9B/IMG-20230703-221517-995.jpg',
-                                       'https://i.ibb.co/nC6HMLs/IMG-20230703-221517-344.jpg']
+m_spawn_krasnogvardeysk_scene_photo = ['https://i.ibb.co/Y3M0fY0/Picsart-23-07-04-16-57-03-077.png',
+                                       'https://i.ibb.co/d2JS1bR/Picsart-23-07-04-16-53-53-757.png',
+                                       'https://i.ibb.co/16DvVX8/Picsart-23-07-04-16-50-51-818.jpg',
+                                       'https://i.ibb.co/8Dxg1t3/Picsart-23-07-04-16-49-23-982.png']
+m_spawn_krasnogvardeysk_scene_pre = 'https://i.ibb.co/8Dxg1t3/Picsart-23-07-04-16-49-23-982.png'
