c01e136ff99ca07dc9c79109263b542cf30bc5c6
zaamilion <107994046+zaamilion@users.noreply.github.com>
2023-07-04 21:55:42 +0000

    wworrrrrrrk

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardBut
 
 from gtts import gTTS
 
+import asyncio
+
 msgp = None
 msg = None
-markup_in = InlineKeyboardMarkup(row_width=3).row(InlineKeyboardButton(text='Спавн', callback_data='m_spawn_'),InlineKeyboardButton(text='exit',callback_data='exit')) 
+markup_in = InlineKeyboardMarkup(row_width=2).row(InlineKeyboardButton(text='🏞️Спавн', callback_data='m_spawn_'),InlineKeyboardButton(text='Красногвардейск',callback_data='m_krasnogvardeysk_')).add(InlineKeyboardButton(text='🚪exit',callback_data='exit')) 
 bot = Bot(token='6213197194:AAGzmFDDnZ-_Py1lgciZQiHfpGOopmNhqGs')
 dp = Dispatcher(bot)
+
 def markup_out(callin):
-    return  InlineKeyboardMarkup(row_width=3).row(InlineKeyboardButton(text='Спавн', callback_data=callin+'spawn_city_pre'),InlineKeyboardButton(text='Сцена Красногвардейск', callback_data=callin+'krasnogvardeysk_scene_pre'),InlineKeyboardButton(text='exit',callback_data='exit'))
+    return  InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='🏞️Спавн', callback_data=callin+'spawn_city_pre')).add(InlineKeyboardButton(text='🏬Сцена Красногвардейск', callback_data=callin+'krasnogvardeysk_scene_pre')).add(InlineKeyboardButton(text='Рынок Альфа',callback_data=callin+'utrecht_alpha_pre')).add(InlineKeyboardButton(text='🚪exit',callback_data='exit'))
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     global msg
-    msg = await message.answer('КУУУ, откуда едем?',reply_markup=markup_in)
+    msg = await message.answer('👋 Приветствую вас в первом навигаторе на #СПб - 7Gis\n\nℹ️ Для начала построения маршрута выберите точку начала',reply_markup=markup_in)
 
 @dp.callback_query_handler()
 async def callb(call: types.CallbackQuery):
@@ -32,7 +35,7 @@ async def callb(call: types.CallbackQuery):
         pass
     if call.data == 'exit':
         await call.answer('restarted')
-        msg = await call.message.answer('КУУУ, откуда едем?',reply_markup=markup_in)
+        msg = await call.message.answer('ℹ️ Для начала построения маршрута выберите точку начала',reply_markup=markup_in)
     elif call.data[:1]=='m':
         print(1)
         try:
@@ -40,25 +43,29 @@ async def callb(call: types.CallbackQuery):
             print(2)
             try:
                 i = (globals()[call.data[:-7]])[step+1]
-                msgp = await call.message.answer_photo((globals()[(call.data[:-7])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Едем дальше',callback_data=call.data[:-2]+str(step+1))))
+                msgp = await call.message.answer_photo((globals()[(call.data[:-7])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='⬆️Едем дальше',callback_data=call.data[:-2]+str(step+1))))
             except:
-                msgp = await call.message.answer_photo((globals()[(call.data[:-7])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Закончить поездку',callback_data=call.data[:-2]+'end')))
+                msgp = await call.message.answer_photo((globals()[(call.data[:-7])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='🚪Закончить поездку',callback_data=call.data[:-2]+'end')))
         except:
             try:
                 step = int(call.data[-1])
                 try:
                     i = (globals()[call.data[:-6]])[step+1]
-                    msgp = await call.message.answer_photo((globals()[(call.data[:-6])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Едем дальше',callback_data=call.data[:-1]+str(step+1))))
+                    msgp = await call.message.answer_photo((globals()[(call.data[:-6])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='⬆️Едем дальше',callback_data=call.data[:-1]+str(step+1))))
                 except:
                     print(3)
-                    msgp = await call.message.answer_photo((globals()[(call.data[:-6])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Закончить поездку',callback_data=call.data[:-2]+'end')))
+                    msgp = await call.message.answer_photo((globals()[(call.data[:-6])+'_photo'])[step],reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='🚪Закончить поездку',callback_data=call.data[:-2]+'end')))
             except:
                 print(call.data[-3:])
                 if call.data[-3:]=='end':
-                    msg = await call.message.answer('поездка успешно завершена',reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Начать новую поездку',callback_data='exit')))
+                    msg = await call.message.answer('✅ Поездка успешно завершена',reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Начать новую поездку',callback_data='exit')))
                 elif call.data[-3:]=='pre':
-                    msgp = await call.message.answer_photo((globals()[call.data]),caption='Маршрут построен',reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='Поехали',callback_data=call.data[:-3]+'step0'),InlineKeyboardButton(text='exit',callback_data='exit')))
+                    msg = await call.message.answer('⏳ построение маршрута')
+                    await asyncio.sleep(0.5)
+                    await msg.delete()
+                    await asyncio.sleep(0.5)
+                    msgp = await call.message.answer_photo((globals()[call.data]),caption='✅ Маршрут построен',reply_markup=InlineKeyboardMarkup(row_width=1).row(InlineKeyboardButton(text='▶️Поехали',callback_data=call.data[:-3]+'step0'),InlineKeyboardButton(text='🚪exit',callback_data='exit')))
                 else:
-                    msg = await call.message.answer('а куда?',reply_markup=markup_out(call.data))
+                    msg = await call.message.answer('ℹ️ выберите точку завершения',reply_markup=markup_out(call.data))
 
 executor.start_polling(dp)
--- routes.py
+++ routes.py
@@ -1,7 +1,12 @@
-m_spawn_krasnogvardeysk_scene = [["spawn","utrecht"],["utrecht","krasnogvardeysk"],
-                                 ["krasnogvardeysk","krasnogvardyesk"],["krasnogvardeysk","krasnogvardeysk_scene"]]
+m_spawn_krasnogvardeysk_scene = [["spawn","utrecht"],["utrecht","m1"],
+                                 ["m1","krasnogvardyesk"],["krasnogvardeysk","krasnogvardeysk_scene"]]
 m_spawn_krasnogvardeysk_scene_photo = ['https://i.ibb.co/Y3M0fY0/Picsart-23-07-04-16-57-03-077.png',
-                                       'https://i.ibb.co/d2JS1bR/Picsart-23-07-04-16-53-53-757.png',
+                                       'https://i.ibb.co/b7NLdHC/Picsart-23-07-04-20-01-13-087.png',
                                        'https://i.ibb.co/16DvVX8/Picsart-23-07-04-16-50-51-818.jpg',
                                        'https://i.ibb.co/8Dxg1t3/Picsart-23-07-04-16-49-23-982.png']
-m_spawn_krasnogvardeysk_scene_pre = 'https://i.ibb.co/8Dxg1t3/Picsart-23-07-04-16-49-23-982.png'
+m_spawn_krasnogvardeysk_scene_pre = 'https://i.ibb.co/b7NLdHC/Picsart-23-07-04-20-01-13-087.png'
+
+m_krasnogvardeysk_utrecht_alpha = [["krasnogvardeysk","m1"],["m1","utrecht_alpha"]]
+m_krasnogvardeysk_utrecht_alpha_photo = ['https://i.ibb.co/v409yWP/Picsart-23-07-04-21-45-44-391.png',
+                                         'https://i.ibb.co/WWv8yKg/Picsart-23-07-04-22-27-02-814.jpg']
+m_krasnogvardeysk_utrecht_alpha_pre  = 'https://i.ibb.co/WFH7Gtq/Picsart-23-07-04-23-25-12-881.png'
